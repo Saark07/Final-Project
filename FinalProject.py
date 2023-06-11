3af89af8b3bbd3bffb1e927e284204d4c8390f15
Saark07 <saarkeshet@gmail.com>
2023-06-11 18:37:08 +0300

    first

---
+++ FinalProject.py
@@ -0,0 +1,231 @@
+import sys
+import csv
+import re
+
+from PyQt5.QtCore import Qt, QFile, QIODevice
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QSizePolicy, QWidget, QMessageBox
+from PyQt5.QtGui import QPixmap, QResizeEvent, QPainter, QIcon
+from PyQt5 import QtWidgets
+
+width = 1100
+height = 750
+edgesDictionary = {}
+idToNames = {}
+idToIdSorted = {}
+nameToId = {}
+
+
+def set_background_image(self, image_path, opacity=1.0):
+    # Create a QPixmap from the image file
+    pixmap = QPixmap(image_path)
+
+    # Resize the pixmap to fit the window size
+    pixmap = pixmap.scaled(self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+
+    # Create a new QPixmap with the desired opacity
+    opacity_pixmap = QPixmap(pixmap.size())
+    opacity_pixmap.fill(Qt.transparent)
+
+    # Set the opacity by manipulating the alpha channel
+    painter = QPainter(opacity_pixmap)
+    painter.setOpacity(opacity)
+    painter.drawPixmap(0, 0, pixmap)
+    painter.end()
+
+    # Set the pixmap as the QLabel's background
+    self.backgroundLabel.setPixmap(opacity_pixmap)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Load the UI form from the .ui file
+        self.load_ui()
+        # Set the window icon
+        # Connect the exitButtonPage1 clicked signal to the application quit
+        self.exitButtonPage1.clicked.connect(QApplication.quit)
+        # Set the background image
+        set_background_image(self, "images/robotic_arm.jpg", opacity=0.65)
+        self.setFixedSize(width, height)
+        # Connect the next button clicked signal to the next page function
+        self.nextButtonPage1.clicked.connect(self.switch_to_page2)
+
+    def load_ui(self):
+        from PyQt5 import uic
+        uic.loadUi("ui_files/page1.ui", self)
+
+    def switch_to_page2(self):
+        page2 = Page2()
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        widget.addWidget(page2)
+        widget.removeWidget(self)
+
+
+class Page2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Load the UI form from the .ui file
+        self.load_ui()
+        set_background_image(self, "images/robotic_arm.jpg", opacity=0.65)
+        self.setFixedSize(width, height)
+        self.searchButtonPage2.clicked.connect(self.switch_to_page3)
+
+        # Connect the returnPressed signal of the line edit to the click slot of the search button
+        self.lineEditPage2.returnPressed.connect(self.searchButtonPage2.click)
+
+        # Connect the exitButtonPage1 clicked signal to the application quit
+        self.exitButtonPage2.clicked.connect(QApplication.quit)
+
+    def load_ui(self):
+        from PyQt5 import uic
+        uic.loadUi("ui_files/page2.ui", self)
+
+    def switch_to_page3(self):
+        text_to_transfer = self.lineEditPage2.text()
+        # print("text to transfer", text_to_transfer)
+        if len(text_to_transfer) == 0:
+            QMessageBox.information(self, "No Text Found", "No text matching the search criteria was found.")
+            return
+        page3 = Page3(text_to_transfer)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        widget.addWidget(page3)
+        widget.removeWidget(self)
+
+
+class Page3(QMainWindow):
+    def __init__(self, transferred_text):
+        super().__init__()
+        self.transferred_text = transferred_text
+        # Load the UI form from the .ui file
+        self.load_ui()
+        set_background_image(self, "images/robotic_arm.jpg", opacity=0.65)
+        self.setFixedSize(width, height)
+
+        # Connect the exitButtonPage1 clicked signal to the application quit
+        self.exitButtonPage3.clicked.connect(QApplication.quit)
+        self.searchButtonPage3.clicked.connect(self.switch_to_page2)
+        self.updateResults()
+
+    def load_ui(self):
+        from PyQt5 import uic
+        uic.loadUi("ui_files/page3.ui", self)
+        # print(self.transferred_text)
+
+    def switch_to_page2(self):
+        page2 = Page2()
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        widget.addWidget(page2)
+        widget.removeWidget(self)
+
+    def updateResults(self):
+        # Search for ID corresponding to the transferred_text
+        id_ = None
+        for name, id_value in nameToId.items():
+            if self.transferred_text.lower() in name.lower():
+                id_ = id_value
+                break
+        if id_ is not None:
+            if id_ == "325497":
+                print("IM HERE")
+            result_names = []
+            # Get the sorted IDs list corresponding to the found ID
+            sorted_ids = idToIdSorted.get(id_)
+            if sorted_ids is not None:
+                for sorted_id in reversed(sorted_ids):
+                    # Get the name corresponding to each sorted ID
+                    if idToNames.get(sorted_id) is not None:
+                        name = idToNames.get(sorted_id)
+                    if name is not None:
+                        result_names.append(name)
+                        name = None
+            if not result_names:
+                self.textEditPage3.append("No papers found.")
+            for name in result_names:
+                name = re.sub(r'"|,$', '', name)
+                new_text = name.rsplit(',', 1)[0] + '.'
+                new_text = new_text.replace("..", ".")
+                self.textEditPage3.append(new_text + '\n')
+        else:
+            self.textEditPage3.append("No papers found.")
+
+
+def LoadData():
+    input_file = "csv_files/reconstructed_edges.csv"
+    with open(input_file, "r") as file:
+        reader = csv.reader(file)
+
+        # Skip the header row
+        next(reader)
+
+        for row in reader:
+            edge = eval(row[0])
+            count = int(row[1])
+            edgesDictionary[edge] = count
+
+    # print(edgesDictionary)
+
+
+def ReadPapersName():
+    import csv
+    filename = "csv_files/paper_names_id.csv"
+
+    with open(filename, "r", newline="") as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip the first row (header)
+
+        for row in csv_reader:
+            if len(row) >= 2:
+                id_ = row[0]
+                name = row[1]
+                idToNames[id_] = name
+                nameToId[name] = id_
+
+
+    # print(nameToId)
+    # print(namesDictionary)
+
+
+def CreatePapersDictionary():
+    for id_, name in idToNames.items():
+        matching_names = []
+        for key, value in edgesDictionary.items():
+            if str(key[0]) == id_:
+                matching_names.append(str(key[1]) + ',' + str(value))
+            if str(key[1]) == id_:
+                matching_names.append(str(key[0]) + ',' + str(value))
+        idToIdSorted[id_] = matching_names
+
+    for key, value in idToIdSorted.items():
+        # Sort the list in ascending order based on the integer value after ','
+        sorted_list = sorted(value, key=lambda x: int(x.split(',')[1]))
+        idToIdSorted[key] = sorted_list
+
+    for key, value in idToIdSorted.items():
+        # Remove '0' values and delete the characters after ','
+        cleaned_list = [item.split(',')[0] for item in value if item.split(',')[1] != '0']
+        idToIdSorted[key] = cleaned_list
+
+    # print(idToIdSorted)
+
+
+
+# Functions that load the data into a dictionaries
+LoadData()
+ReadPapersName()
+CreatePapersDictionary()
+
+
+
+# Run the App\GUI
+app = QApplication(sys.argv)
+widget = QtWidgets.QStackedWidget()
+window = MainWindow()
+icon = QIcon("images/icon.png")
+widget.setWindowIcon(icon)
+widget.setWindowTitle("Final Project")
+widget.addWidget(window)
+widget.setFixedSize(width, height)
+widget.show()
+sys.exit(app.exec_())
---
+++ specfile.spec
@@ -0,0 +1,57 @@
+# specfile.spec
+
+# Import the required module
+import sys
+from PyQt5.Qt import *
+
+# Define the main script
+a = Analysis(['FinalProject.py'],
+             pathex=[],
+             binaries=[],
+             datas=[('ui_files/page1.ui', 'ui_files'),
+                    ('ui_files/page2.ui', 'ui_files'),
+                    ('ui_files/page3.ui', 'ui_files'),
+                    ('images/icon.png', 'images'),
+                    ('images/icon.ico', 'images'),
+                    ('csv_files/paper_names_id.csv', 'csv_files'),
+                    ('csv_files/reconstructed_edges.csv', 'csv_files'),
+                    ('images/robotic_arm.jpg', 'images')],  # Add the image file entry here
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=None)
+
+# Create the executable
+pyz = PYZ(a.pure, a.zipped_data,
+          cipher=None)
+
+# Create the EXE
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          [],
+          name='FinalProject',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          upx_exclude=[],
+          runtime_tmpdir=None,
+          icon='images/icon.ico',
+          console=False)
+
+# Delete unnecessary files
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               upx_exclude=[],
+               name='FinalProject')
+
